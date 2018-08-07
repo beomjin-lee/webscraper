708eba92cca4fe18a9e4fbd7aad088490f6e638f
beomjin-lee <cluesbj@gmail.com>
2018-08-07 12:02:38 -0700

    changes to webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -2,7 +2,7 @@
 Webscraper
 ----------
 
-Template for web-scraping PDF documentsself.
+Template for web-scraping PDF documents.
 UC Berkeley's EECS70 Notes is given as an example.
 
 Author
@@ -11,21 +11,31 @@ Beom Jin Lee <cluesbj@berkeley.edu>
 """
 
 import requests
+import os
+import logging
 
 
-def download_file(download_url, output_path):
-    print("Downloading from ", download_url)
+def download_file(download_url, output_directory, prefix="note", file_type=".pdf"):
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    output_name = output_directory + prefix + file_type
+
+    logging.info("Downloading from ", download_url)
+
     response = requests.get(download_url, stream=True)
-    file = open(output_path, 'wb')
+    file = open(output_name, 'wb')
     file.write(response.content)
     file.close()
-    print("Completed Downloading")
+
+    logging.info("Completed Downloading")
 
 
 def main():
     for i in range(1, 27):
+        prefix = "note" + str(i)
         download_file("http://www.eecs70.org/static/notes/n{}.pdf".format(i),
-                      "~/Desktop/cs70/note{}.pdf".format(i))
+                      "/Users/beomjin_lee/Desktop/cs70/", prefix, file_type=".pdf")
 
 
 if __name__ == "__main__":
